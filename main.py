a1f6d3252c2bbdbc98f3c13c57a38bd01a1e2ef7
tarungujral156 <tarungujral156@gmail.com>
2026-02-02 01:56:02 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.cnnClassifier import logger
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcom to our custom log")
+
+STAGE_NAME= "Data Ingesetion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nX===========X")
+except Exception as e:
+    logger.exception(e)
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -47,7 +47,7 @@ def create_directories(path_to_directories: list, verbose=True):
         ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
-        os.makedirs(path, exist_ok=True):
+        os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directrory at: {path}")
             
